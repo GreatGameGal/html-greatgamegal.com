95953283a9454aed0fd2c384fbc62fd9ec10a178
greatgamegal <thegreatgamegal@gmail.com>
2020-09-10 14:21:12 -0700

    Added seeded Random and used it for perlin noise permutation generation.

--- js/random.js
+++ js/random.js
@@ -1,5 +1,17 @@
 class Random {
   constructor(seed) {
+    this.seed = seed || Math.floor(Math.random()*2147483647);
+  }
+
+  random() {
+    this.localSeed |= 0;
+    this.localSeed = this.localSeed + 0x6D2B79F5 | 0;
+    let t = Math.imul(this.localSeed ^ this.localSeed >>> 15, 1 | this.localSeed);
+    t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
+    return ((t^t >>> 14) >>> 0) / 4294967296;
+  }
+
+  setSeed(seed) {
     this.seed = seed;
   }
 
