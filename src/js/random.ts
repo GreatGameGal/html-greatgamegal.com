8c8d6bc7f837d987355e03df65aa7178b0b43867
Serenity Braesch <serenitybraesch03@gmail.com>
2023-09-29 16:01:47 -0700

    Add .next for integers and use .next for .random

--- src/js/random.ts
+++ src/js/random.ts
@@ -1,26 +1,30 @@
 class Random {
-  private localSeed: number;
+  private state: number;
   private seed: number;
 
   constructor (seed?: number) {
     this.seed = seed ?? Math.floor(Math.random() * 2147483647);
-    this.localSeed = this.seed;
+    this.state = this.seed;
+  }
+
+  next () {
+    this.state |= 0;
+    this.state = this.state + 0x6d2b79f5 | 0;
+    let t = Math.imul(
+      this.state ^ this.state >>> 15,
+      1 | this.state
+    );
+    t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
+    return (t ^ t >>> 14) >>> 0;
   }
 
   random () {
-    this.localSeed |= 0;
-    this.localSeed = this.localSeed + 0x6d2b79f5 | 0;
-    let t = Math.imul(
-      this.localSeed ^ this.localSeed >>> 15,
-      1 | this.localSeed
-    );
-    t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    return this.next() / 4294967296;
   }
 
   setSeed (seed: number) {
     this.seed = seed;
-    this.localSeed = seed;
+    this.state = seed;
   }
 
   getSeed () {
